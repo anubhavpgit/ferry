e360b2981bb4aec6c383ab75b02e88a1d865d691
Anubhab Patnaik <anubhabr50@gmail.com>
2025-03-24 16:21:17 -0400

    refactor: added placeholder for mac_os_runner

---
+++ src/mac_os_runner.rs
@@ -0,0 +1,6 @@
+// This file is specifically for Mac OS to use cross-platform assembler for RISC-V.
+// brew install riscv-tools
+// brew install qemu
+fn main() {
+    println!("assemble the argument file and use a linker and run the executable using quemu !");
+}
